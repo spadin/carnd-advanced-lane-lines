0e48cfdc0838af42ffab2249879dd5336eb474ab
Sandro Padin <sandropadin@gmail.com>
2017-01-22 18:12:11 -0800

    Add help for converting rgb to hls

--- image_helper.py
+++ image_helper.py
@@ -8,3 +8,7 @@ def rgb_image(filepath):
 def rgb_to_gray(img):
     """Convert an RGB image to Grayscale"""
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+def rgb_to_hls(img):
+    """Convert an RGB image to HLS"""
+    return cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
